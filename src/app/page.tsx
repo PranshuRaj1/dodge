e2d871eba2a1614af7a175ee3c0f32b2964266c2
Pranshu Raj <rajpranshu2222@gmail.com>
2026-03-23 15:48:41 +0530

    as SQL commands works fine now i am commenting this component until i found any bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default async function HomePage() {
         <div className="chat-divider" />
 
         {/* Chat pane */}
-        <ChatPanel />
+        {/* <ChatPanel /> */}
       </main>
     </div>
   );
